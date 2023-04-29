d203f1fef4b8c0661f2d35acfe8ac3c7cf8bc879
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2023-04-28 18:39:37 -0700

    Update WgetHistory.py

--- ALEAPP/WgetHistory.py
+++ ALEAPP/WgetHistory.py
@@ -15,7 +15,7 @@ def get_WgetHistory(files_found, report_folder, seeker, wrap_text):
         report = ArtifactHtmlReport('WGET History')
         report.start_artifact_report(report_folder, f'WGET History')
         report.add_script()
-        data_headers = ('Order', 'Command')
+        data_headers = ('Order', 'URL downloaded')
         report.write_artifact_data_table(data_headers, data_list, file_found)
         report.end_artifact_report()
         
